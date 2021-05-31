792c66ea87802357db8f00e645ad3df22ea05742
‘lb’ <1012514235@qq.com>
2021-05-31 10:45:20 +0800

    20210531-1

---
+++ testMultiThreads.py
@@ -0,0 +1,44 @@
+from time import sleep, ctime
+import threading
+
+loops = [6, 4, 2, 8]
+
+
+class MyThread(threading.Thread):
+    def __init__(self, func, args, name=''):
+        super().__init__()
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def run(self):
+        return self.func(*self.args)
+
+
+def loop(nloop, nsec):
+    print('start loop', nloop, 'at:', ctime())
+    sleep(nsec)
+    print('loop', nloop, 'done at:', ctime())
+
+
+def main():
+    print('starting at:', ctime())
+    threads = []
+    nloops = range(len(loops))
+
+    for i in nloops:
+        t = MyThread(loop, (i, loops[i]), loop.__name__)
+        threads.append(t)
+
+    for i in nloops:
+        threads[i].start()
+
+    for i in nloops:
+        threads[i].join()
+        print(f'for loop {i} DONE')
+
+    print('all DONE at:', ctime())
+
+
+if __name__ == '__main__':
+    main()
